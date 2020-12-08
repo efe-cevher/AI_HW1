ef2e69c9bff666f13a9a829428150aae878b5ed8
Efe Cevher <37309169+efe-cevher@users.noreply.github.com>
2020-12-08 03:23:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import sys
 data_file_name = sys.argv[1]
 clue_file_name = sys.argv[2]
 
-data_input_str = open("files/" + data_file_name).read()
-clue_input_str = open("files/" + clue_file_name).read()
-
-data_input_list = data_input_str.split("\n")
-clue_input_list = clue_input_str.split("\n")
+data_input_list = open("files/" + data_file_name).read().split("\n")
+clue_input_list = open("files/" + clue_file_name).read().split("\n")
 
 data = {}
+constraints = []
 
 for line in data_input_list:
     attr_input_list = line.split(",")
@@ -17,11 +15,12 @@ for line in data_input_list:
     domains = attr_input_list[1:]
     data[attr_name] = domains
 
-constraints = []
-
-
 for line in clue_input_list:
-    line_elements = line.split(",")
+
+    if not len(line) > 1:
+        break
+
+    line_elements = line.split(" ")
     if line_elements[0] == "if":
         equality = line_elements[1].split('=')
         x = equality[0]
@@ -32,7 +31,7 @@ for line in clue_input_list:
             y = equality[0]
             b = equality[1]
 
-            constraint = lambda s : s[x]==a and s[y]!=b
+            constraint = lambda s : s[x]!=a or s[y]!=b
 
         elif line_elements[3] == "either":
             equality = line_elements[4].split('=')
@@ -43,15 +42,14 @@ for line in clue_input_list:
             z = equality[0]
             c = equality[1]
 
-            constraint = lambda s : s[x]==a and (s[y]==b ^ s[z]==c)
+            constraint = lambda s : s[x]!=a or (s[y]==b ^ s[z]==c)
         
         else:
             equality = line_elements[3].split('=')
             y = equality[0]
             b = equality[1]
 
-            constraint = lambda s : s[x]==a and s[y]==b
-
+            constraint = lambda s : s[x]!=a or s[y]==b
 
     elif line_elements[0] == "one":
         equalities = line_elements[2][1:-1].split(',')
@@ -72,7 +70,7 @@ for line in clue_input_list:
         t = equality[0]
         d = equality[1]
 
-        constraint = lambda s1,s2 : s1!=s2 and ((s1[x]==a and s1[z]==c and s2[y]==b and s2[t]==d) ^ (s1[x]==a and s1[t]==d and s2[y]==b and s2[z]==c))
+        constraint = lambda s1,s2 : (s1==s2) or (s1[x]!=a or s2[y]!=b) or (s1[z]==c and s2[t]==d) or (s1[t]==d and s2[z]==c)
 
     elif line_elements[0][0] == "{":
         equalities = line_elements[0][1:-1].split(',')
@@ -89,7 +87,7 @@ for line in clue_input_list:
         z = equality[0]
         c = equality[1]
         
-        constraint = lambda s1,s2,s3 : s1[x]==a and s2[y]==b and s3[z]==c and s1!=s2 and s2!=s3 and s1!=s3
+        constraint = lambda s1,s2,s3 : (not (s1[x]==a and s2[y]==b and s3[z]==c)) or (s1!=s2 and s2!=s3 and s1!=s3)
 
     else:
         tmp = line_elements[0].split('(')
@@ -105,8 +103,7 @@ for line in clue_input_list:
             y = equality[0]
             b = equality[1]
 
-            constraint = lambda s1,s2 : s1[x]==a and s2[y]==b and s1[n1]>s2[n2]
-            
+            constraint = lambda s1,s2 : (s1[x]!=a or s2[y]!=b) or s1[n1]>s2[n2]  
 
         elif line_elements[1] == '<':
             tmp = line_elements[2].split('(')
@@ -115,8 +112,7 @@ for line in clue_input_list:
             y = equality[0]
             b = equality[1]
 
-            constraint = lambda s1,s2 : s1[x]==a and s2[y]==b and s1[n1]<s2[n2]
-            
+            constraint = lambda s1,s2 : (s1[x]!=a or s2[y]!=b) or s1[n1]<s2[n2]
 
         else:
             tmp = line_elements[2].split('(')
@@ -126,15 +122,15 @@ for line in clue_input_list:
             b = equality[1]
 
             if len(line_elements) > 3:
-                m = line_elements[4]
+                m = int(line_elements[4])
 
                 if line_elements[3] == '+':
-                    constraint = lambda s1,s2 : s1[x]==a and s2[y]==b and s1[n1]==s2[n2] + m
+                    constraint = lambda s1,s2 : (s1[x]!=a or s2[y]!=b) or s1[n1]==s2[n2]+m
                     constraints.append(constraint)
                 else:
-                    constraint = lambda s1,s2 : s1[x]==a and s2[y]==b and s1[n1]==s2[n2] - m
+                    constraint = lambda s1,s2 : (s1[x]!=a or s2[y]!=b) or s1[n1]==s2[n2]-m
                     constraints.append(constraint)
             else:
-                constraint = lambda s1,s2 : s1[x]==a and s2[y]==b and s1[n1]==s2[n2]
+                constraint = lambda s1,s2 : (s1[x]!=a or s2[y]!=b) or s1[n1]==s2[n2]
             
     constraints.append(constraint)
